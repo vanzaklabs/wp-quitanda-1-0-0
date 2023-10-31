ec1514349f189774d386921328edb38160e6355d
Wrootz <vanzak.devhub@gmail.com>
2023-10-31 06:45:13 -0300

    capa

--- page-content-cover.php
+++ page-content-cover.php
@@ -1,35 +1,88 @@
-<?php
-/*
-Template Name: Texto + Capa
-*/
-?>
+<?php get_header(); ?>
 
-<?php
-	get_header('stat');
-?>	
-
-	
-	<section id="capa-conteudo-quitanda" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/capa-conteudo-sdw.png')">
-	<!-- https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/ -->
-	<?php if (has_post_thumbnail()) {
-							// $img_bg = the_post_thumbnail();
-							$img_bg = get_the_post_thumbnail_url();
-							$titulo_capa = get_the_title(); } 
-							?>
-			<?php 
-			echo "<div id='capa-conteudo' style='background-image: url(". $img_bg . ";'>" . "<h1 id='capa-titulo'>" . $titulo_capa . "</h1></div>"; 
-			?>
-	</section>
-	
-
-	<!-- MAIN -->
-	<main id="conteudo-quitanda">
-		<p>
-			<?php 
-				the_content();
-			?>
-		</p>
-	</main>
+<!-- PAGE.PHP -->
 
 
-<?php get_footer();
+    <?php if (has_post_thumbnail()) : $url = get_the_post_thumbnail_url(); ?>
+
+        <div id="capaTituloH1">
+        <h1 id="h1Titulo"><?php the_title(); ?></h1>
+        </div>
+        
+        <div id="capa">
+            <?php echo ' <div class="capaConteudo" style="background: url(' . $url . '); background-size: cover; background-attachment: fixed;"> '; ?>
+            <div id="capaTitulo">
+                
+            </div>
+            <?php echo '</div>' ?>
+        </div>
+    <?php endif; ?>
+
+<main
+id="page-main-content"
+class="d-flex justify-center align-center flex-column p-section bg-03 ">
+    <section id="sec-wpTheLoop" class="container">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                
+                <article id="artigo-wpTheLoop">
+
+                    <div class="text-p">
+                        <?php the_content(); ?>
+                    </div>
+
+                </article>
+
+            <?php endwhile;
+
+        else : ?>
+        
+            <article>
+                <p>:-[ Ué... Não tem nada aqui! </p>
+            </article>
+
+        <?php endif; ?>
+    </section>
+</main>
+
+<style>
+
+    div#capa {
+        display: grid;
+        padding-top: 130px;
+        background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.70) 100%);
+    }
+    .capaConteudo {
+        height: 600px;
+        mix-blend-mode: multiply;
+    }
+    div#capaTitulo {
+        max-width: 90%;
+        display: flex;
+        margin: auto;
+        text-align: -webkit-center;
+        height: 600px;
+        align-items: flex-end;
+    }
+    div#capaTituloH1 {
+        position: absolute;
+        z-index: 1;
+        width: 100%;
+        margin-top: 450px;
+    }
+    section#sec-wpTheLoop {
+        min-height: 60vh;
+        padding-top: 130px;
+    }
+
+    h1#h1Titulo {
+        color: white;
+        padding-bottom: 0px;
+    }
+    h2, div.text-p p {
+        margin-bottom: 25px;
+    }
+
+</style>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -4,10 +4,15 @@
 
 
     <?php if (has_post_thumbnail()) : $url = get_the_post_thumbnail_url(); ?>
+
+        <div id="capaTituloH1">
+        <h1 id="h1Titulo"><?php the_title(); ?></h1>
+        </div>
+        
         <div id="capa">
             <?php echo ' <div class="capaConteudo" style="background: url(' . $url . '); background-size: cover; background-attachment: fixed;"> '; ?>
             <div id="capaTitulo">
-                <h1><?php the_title(); ?></h1>
+                
             </div>
             <?php echo '</div>' ?>
         </div>
@@ -41,18 +46,39 @@ class="d-flex justify-center align-center flex-column p-section bg-03 ">
 </main>
 
 <style>
+
     div#capa {
         display: grid;
         padding-top: 130px;
+        background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.70) 100%);
     }
     .capaConteudo {
         height: 600px;
+        mix-blend-mode: multiply;
+    }
+    div#capaTitulo {
+        max-width: 90%;
+        display: flex;
+        margin: auto;
+        text-align: -webkit-center;
+        height: 600px;
+        align-items: flex-end;
+    }
+    div#capaTituloH1 {
+        position: absolute;
+        z-index: 1;
+        width: 100%;
+        margin-top: 450px;
     }
     section#sec-wpTheLoop {
         min-height: 60vh;
         padding-top: 130px;
     }
 
+    h1#h1Titulo {
+        color: white;
+        padding-bottom: 0px;
+    }
     h2, div.text-p p {
         margin-bottom: 25px;
     }
