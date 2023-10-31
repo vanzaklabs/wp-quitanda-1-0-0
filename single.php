a2fc8e50fd405c4cd72c8e104fa326d4466d691f
Wrootz <vanzak.devhub@gmail.com>
2023-10-31 03:58:32 -0300

    single.php

--- single.php
+++ single.php
@@ -1,21 +1,66 @@
 <?php get_header(); ?>
 
-<main>
-    <section>
+<!-- SINGLE.PHP -->
+
+
+    <?php if (has_post_thumbnail()) : $url = get_the_post_thumbnail_url(); ?>
+        <div id="capa">
+            <?php echo ' <div class="capaConteudo" style="background: url(' . $url . '); background-size: cover; background-attachment: fixed;"> '; ?>
+            <div id="capaTitulo">
+                <h1><?php the_title(); ?></h1>
+            </div>
+            <?php echo '</div>' ?>
+        </div>
+    <?php endif; ?>
+
+<main
+id="page-main-content"
+class="d-flex justify-center align-center flex-column p-section bg-03 ">
+    <section id="sec-wpTheLoop" class="container">
+        <?php if (!has_post_thumbnail()) : ?>
+            <?php echo '<h1 id="text-h1">' . get_the_title() . '</h1>'; ?>
+        <?php endif ?>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article>
-                    <header>
-                        <h2><?php the_title(); ?></h2>
-                        By: <?php the_author(); ?>
-                    </header>
-                    <?php the_content(); ?>
+                
+                <article id="artigo-wpTheLoop">
+
+                    <div class="text-p">
+                        <?php the_content(); ?>
+                    </div>
+
                 </article>
-            <?php endwhile; else : ?>
+
+            <?php endwhile;
+        else : ?>
             <article>
-                <p>Não há conteúdo para ser exibido aqui!</p>
+                <p>:-[ Ué... Não tem nada aqui! </p>
             </article>
+
         <?php endif; ?>
     </section>
 </main>
+
+<style>
+    div#capa {
+        display: grid;
+        padding-top: 130px;
+    }
+    .capaConteudo {
+        height: 600px;
+        /* display: flex; */
+    }
+    section#sec-wpTheLoop {
+        min-height: 60vh;
+        padding-top: 130px;
+    }
+
+    h2 {
+        margin-bottom: 25px;
+    }
+
+    /* p {
+        margin-bottom: 50px;
+    } */
+</style>
 
 <?php get_footer(); ?>
