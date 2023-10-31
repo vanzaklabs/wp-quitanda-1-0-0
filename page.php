e661dde1dd54337d663470220f56c518e1728f97
Wrootz <vanzak.devhub@gmail.com>
2023-10-31 03:58:22 -0300

    capaConteudo

--- page.php
+++ page.php
@@ -5,38 +5,33 @@
 
     <?php if (has_post_thumbnail()) : $url = get_the_post_thumbnail_url(); ?>
         <div id="capa">
-            <?php
-            echo '
-            <div class="capaConteudo"
-            style="background: url(' . $url . ');
-            background-size: cover;
-            background-attachment: fixed;">
-            '; ?>
+            <?php echo ' <div class="capaConteudo" style="background: url(' . $url . '); background-size: cover; background-attachment: fixed;"> '; ?>
             <div id="capaTitulo">
                 <h1><?php the_title(); ?></h1>
             </div>
+            <?php echo '</div>' ?>
         </div>
-        <?php echo '</div>' ?>
-    <?php else : ?>
-        <?php echo '<h1>' . the_title() . '</h1>'; ?>
     <?php endif; ?>
-
 
 <main
 id="page-main-content"
 class="d-flex justify-center align-center flex-column p-section bg-03 ">
     <section id="sec-wpTheLoop" class="container">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                
                 <article id="artigo-wpTheLoop">
 
                     <div class="text-p">
                         <?php the_content(); ?>
                     </div>
+
                 </article>
 
             <?php endwhile;
+
         else : ?>
+        
             <article>
                 <p>:-[ Ué... Não tem nada aqui! </p>
             </article>
