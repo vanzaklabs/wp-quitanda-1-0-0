b5b540e187c3baa9505e661e7abeca7cf25af363
Wrootz <vanzak.devhub@gmail.com>
2023-11-03 01:59:24 -0300

    index.php
    Content

--- index.php
+++ index.php
@@ -3,18 +3,15 @@
 <!-- INDEX.PHP -->
 
 <main class="conteudo-main">
+
+    <h1>
+        <?php bloginfo('name');?>
+            <span class="subtitulo">
+                <?php wp_title(''); ?>
+            </span>
+    </h1>
+
     <section class="conteudo conteudo-wpTheLoop">
-
-        <h1>
-            <?php bloginfo('name');?>
-                <span class="subtitulo">
-                    <?php wp_title(''); ?>
-                </span>
-        </h1>
-
-        <div>
-
-        </div>
         
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -22,15 +19,22 @@
 
                 <?php if ( has_post_thumbnail() ) { ?>
 
+                    <div id="capa-gradiente">
+                        <div class="conteudo-txt">
+                                <h2>
+                                    <a
+                                        id="conteudo-link"
+                                        href="<?php the_permalink(); ?>"
+                                        title="<?php the_title_attribute(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h2>
+                                <?php the_excerpt(); ?>
+                        </div>
                     <div id="capa-artigo" style="background-image: url('<?php the_post_thumbnail_url( null, 'medium' ); ?>');">
-                        <h2>
-                            <a
-                                href="<?php the_permalink(); ?>"
-                                title="<?php the_title_attribute(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <?php the_excerpt(); ?>
+                   
+                        
+                    </div> 
                     </div>
 
                 <?php } ?>
@@ -50,6 +54,7 @@
     main.conteudo-main {
         min-height: 70vh;
         padding-top: 170px;
+        padding-bottom: 30px;
         background-color: var(--bg-03);
     }
     span.subtitulo {
@@ -64,14 +69,29 @@
     }
     article#card-artigo {
         margin-bottom: 50px;
+    }
+    #capa-gradiente{
         background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.70) 100%);
     }
     #capa-artigo {
+        display: flex;
         background-position: center;
         background-size: cover;
         width: 100%;
         height: 500px;
         mix-blend-mode: multiply;
+    }
+    a#conteudo-link {
+        color: white;
+        font-size: 50px;
+    }
+    .conteudo-txt {
+        margin: 80px;
+        width: 80%;
+        padding-top: 250px;
+        color: white;
+        position: absolute;
+        z-index: 1;
     }
 </style>
 <?php get_footer(); ?>
