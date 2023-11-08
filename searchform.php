c2335fee3eb84e2a53ae986a73f61631ecfdc7aa
Vanzak Labs <flavio@vanzaklabs.com.br>
2023-11-08 18:54:03 -0300

    searchform

--- searchform.php
+++ searchform.php
@@ -12,7 +12,14 @@
 
         <label for="search">Buscar</label>
         
-        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+        <input list="s" type="text" name="s" id="search" value="<?php the_search_query(); ?>" autofocus required />
+        <datalist id="s">
+            <option value="Encomendas">
+            <option value="Quitanda Empresas">
+            <option value="Quitanda Escolas">
+            <option value="Trabalhe Conosco">
+            <option value="Contato">
+        </datalist>
         <input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search.png" />
 
         <!-- <input type="hidden" value="receitas" name="post_type" /> -->
@@ -20,3 +27,16 @@
     </form>
 
 </section>
+
+<style>
+    input#search {
+        height: 150px;
+        width: 510px;
+        color: white;
+        background-color: transparent;
+        border: 0px;
+    }
+    input#search:focus {
+        border: 0px;
+    }
+</style>
